c3ac8fc012e176238125c2b8fee7108a532ae3ea
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-09 00:40:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,16 @@
 def my_collect(array)
+  i = 0
+  collection = []
+  while
+end
 
+
+#def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
